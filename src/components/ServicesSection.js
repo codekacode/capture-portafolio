0b7bd3324071b4dee2f0b83595fca296c4f1a45f
Codekacode <karem.quiroz.m@gmail.com>
2021-06-17 18:46:18 -0500

    Add Section service

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,50 @@
+import clock from '../img/clock.svg';
+import diaphgram from '../img/diaphragm.svg';
+import money from '../img/money.svg';
+import teamwork from '../img/teamwork.svg';
+import home2 from '../img/home2.png';
+
+const ServicesSection = () => {
+  return(
+    <div className="services">
+      <div className="description">
+        <h2>High <span>quality</span> service.</h2>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={clock} />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem Ipsum dolor sit.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={diaphgram} />
+              <h3>Diaphgram</h3>
+            </div>
+            <p>Lorem Ipsum dolor sit.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={money} />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem Ipsum dolor sit.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={teamwork} />
+              <h3>Team Work</h3>
+            </div>
+            <p>Lorem Ipsum dolor sit.</p>
+          </div>
+        </div>
+      </div>
+      <div className="image">
+        <img src={home2} />
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
