a6e76545b40bed7b7a3e0cc5e052d628039ddf2c
Codekacode <karem.quiroz.m@gmail.com>
2021-06-27 18:33:50 -0500

    MAke reusable component Toggle

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,26 +1,32 @@
+import {useState} from "react";
 import styled from "styled-components";
-import {Layout} from '../style'
+import {Layout} from '../style';
+import Toggle from "./Toggle"
 
 const FaqSection = () => {
   return(
     <Faq>
       <h2>Any Questions <span>FAQ</span></h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet, consectetur jahdja</p>
-          <p>Lorem ipsum dolor sit amet, consectetur jahdja Nececitabu haba</p>
+      <Toggle>
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet, consectetur jahdja</p>
+            <p>Lorem ipsum dolor sit amet, consectetur jahdja Nececitabu haba</p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet, consectetur jahdja</p>
-          <p>Lorem ipsum dolor sit amet, consectetur jahdja Nececitabu haba</p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>Daily Schedule</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet, consectetur jahdja</p>
+            <p>Lorem ipsum dolor sit amet, consectetur jahdja Nececitabu haba</p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
+      </Toggle>
       <div className="question">
         <h4>Diferent Payment</h4>
         <div className="answer">
