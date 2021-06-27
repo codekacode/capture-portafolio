2442b5ec917d0002d5d24d58841d9f41575fef2b
Codekacode <karem.quiroz.m@gmail.com>
2021-06-27 00:53:00 -0500

    Add background our work white

--- src/pages/ConstactUs.js
+++ src/pages/ConstactUs.js
@@ -1,9 +1,16 @@
+import {motion} from "framer-motion";
+import {pageAnimation} from "../animation"
 
 const ContactUs = () => {
   return(
-    <div>
+    <motion.div
+      exit="exit" 
+      variants={pageAnimation} 
+      initial="hidden" 
+      animate="show"
+      style={{background: "#fff"}}>
       <h1>Constact Us</h1>
-    </div>
+    </motion.div>
   )
 }
 
